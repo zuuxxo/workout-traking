fc38226544ce29f6e00078c857c6eaaf9d3f6f5f
Olivier Chanteau <chanteau.o@gmail.com>
2023-07-26 23:59:26 +0200

    creation des differents use case

---
+++ src/app/domain/features/workout-session-model/display-workout-session-models-use-case.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class DisplayWorkoutSessionModelsUseCase {
+
+  execute() {
+    //   je veux afficher tous les models de session disponible
+    // je les recupere du store
+    //   ils sont recuperer du back au chargement de l appli
+    // je souhaite avoir un entrainement toujours existant qui ^permette de commmencer une session a vide
+    //
+
+
+  }
+}
